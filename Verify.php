be8d5f6e847176fb03e12e35a7b55f4e00c4f4a5
yousseftawfik11 <67921054+yousseftawfik11@users.noreply.github.com>
2021-11-21 01:39:52 +0200

    change pass and verify with small bug

--- Verify.php
+++ Verify.php
@@ -1,52 +1,96 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+    include 'db.php';
+    if (isset($_POST['submit3'])) {
+        session_start();
+        $_SESSION["username"] = $_POST['studentID'];
+        $StudentID = mysqli_real_escape_string($conn, $_POST['studentID']);
+        $GettingEmail = "SELECT email FROM student WHERE studentID='$StudentID' ";
+        $GetEmail = mysqli_query($conn, $GettingEmail);
+
+        if(mysqli_num_rows($GetEmail)>0){
+
+            $Row = mysqli_fetch_row($GetEmail);
+            $VerificationCode = mt_rand(1111, 9999);
+            $ver = $VerificationCode;
+            echo $ver;
+            $subject = "Verification Code";
+            $txt = "Your verification code is:" . $VerificationCode;
+            mail($Row[0], $subject, $txt, 'From: rahwyco@gmail.com'); //The email function
+    
+            echo '
+            <div class="alert alert-success alert-dismissible" style= "margin-bottom:0px;">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+    <strong>Email was sent successfully!</strong> Please write the verification code in the field below.
+  </div>';
+        }else{
+            echo '
+            <div class="alert alert-danger alert-dismissible" style= "margin-bottom:0px;>
+            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+            <strong>User ID not found!</strong> Please check your entry again.
+          </div>';
+        }
+        
+    }
+    if (isset($_POST['submit4'])) {
+        if ($_POST['Verification'] == "abc") {
+
+            echo '
+	<script>
+	window.location.href="changePassword.php";
+	</script>
+  ';;
+        }
+    }
+
+
+    ?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="loginStyle.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
+
 <body>
     
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-<label for="studentID">Student ID</label>
-<input id="studentID" name="studentID"> </input>
-<button id="submit3" name="submit3">Submit</button>
-</form>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-<label for="Verification">Verification Code</label>
-<input id="Verification" name="Verification"> </input>
-<button id="submit4" name="submit4">Submit</button>
-</form>
-<?php
-include 'db.php';
-if (isset($_POST['submit3'])) {
-    session_start();
-    $_SESSION["username"] = $_POST['studentID'];
-	$StudentID = mysqli_real_escape_string($conn, $_POST['studentID']);
-    $GettingEmail= "SELECT email FROM student WHERE studentID='$StudentID' ";
-    $GetEmail= mysqli_query($conn,$GettingEmail);
-    $Row = mysqli_fetch_row($GetEmail);   
-    echo "Email was sent!";
+    <div class="login-wrap">
+        <div class="login-html">
+            <div class="login-form">
+
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                    <div class="group">
+                        <div class="tasksInput">
+                            <label for="studentID" class="label">Student ID</label>
+                            <input id="studentID" name="studentID" class="input"> </input>
+                        </div>
+                    </div>
+                    <div class="group">
+                        <button id="submit3" name="submit3" class="button" style="margin-bottom: 50px;">Get Code</button>
+                    </div>
+                </form>
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                <div class="group">
+                        <div class="tasksInput">
+                    <label for="Verification" class="label">Verification Code</label>
+                    <input id="Verification" name="Verification" class="input"> </input>
+                    </div>
+                    </div>
+                    <div class="group">
+                    <button id="submit4" name="submit4" class="button">Check</button>
+                    </div>
+                </form>
+
+            </div>
+
+        </div>
+    </div>
     
-    $VerificationCode = mt_rand(1111,9999);
-    $subject = "Verification Code";
-    $txt = "Your verification code is:" . $VerificationCode;
-    mail($Row[0], $subject, $txt, 'From: rahwyco@gmail.com');//The email function
-}
-if (isset($_POST['submit4'])) {
-    if($_POST['Verification']=="abc"){
-        
-        echo '
-	<script>
-	window.location.href="changePassword.php";
-	</script>
-  ';;
-    }
-}
-
-
-?>
-
 </body>
+
 </html>
--- changePassword.php
+++ changePassword.php
@@ -4,15 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="loginStyle.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <title>Document</title>
 </head>
 <body>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        
-        <label for="newPass">New Password</label>
-        <input id="newPass" name="newPass"> </input>
-        <button id="submit5" name="submit5">Submit</button>
-    </form>
+
+
+
+
 <?php
 include 'db.php';
 session_start();
@@ -25,7 +27,13 @@ if (isset($_POST['submit5'])) {
     $changed= mysqli_query($conn,$Upadtepass);
     
     if($changed){
-        echo "Succesfully changed password";
+        echo '
+        <div class="alert alert-success alert-dismissible" style= "margin-bottom:0px;">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<strong>Password was changed successfullt!</strong> You can login using your new password now.
+</div>';
+
+
     }else{
         echo "error";
     }
@@ -34,5 +42,40 @@ if (isset($_POST['submit5'])) {
 
 
 ?>
+
+
+<div class="login-wrap">
+        <div class="login-html">
+            <div class="login-form">
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+<div class="group">
+                        <div class="tasksInput">
+        <label for="newPass" class="label">New Password</label>
+        <input id="newPass" name="newPass" class="input"> </input>
+        </div>
+</div>
+<div class="group">
+        <button id="submit5" name="submit5" class="button">Submit</button>
+</div>     
+<div class="hr" style="margin: 26px 0 33px 0;"></div>
+						<div class="foot-lnk">
+							<a href="login.php">To Login!</a>
+						</div>         
+    </form>
+   
+    
+
+            </div>
+        </div>
+</div>
+
+
+
+
+
+
+
+
 </body>
 </html>
